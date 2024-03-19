4b20fa54df9003fc5d8d68393fd36a8eb9b80d28
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-20 00:28:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+const table = document.querySelector('myTable')
 function createTable() {
     //Write your code here
-  
+  let rows = prompt('Input number of rows')
+let cols = prompt('Input number of colunms')
+for(let i = 0; i<rows; i++){
+    const row = document.createElement('tr')
+    for(let j=0; j<cols; j++){
+       let cell =  document.createElement('td')
+       cell.innerText = `Row-${i} Colums-${j}`
+       row.append(cell)
+    }
+    table.append(row)
+}
 }
